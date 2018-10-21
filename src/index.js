2693baed2d4432e1db967925b6cd4023bd4f62c0
Greg Wiley <gwiley@cj.com>
2018-10-20 17:37:06 -0700

    cleanup

--- src/index.js
+++ src/index.js
@@ -11,15 +11,7 @@ const Schema = `
         query: Query
         mutation: Mutation
     }
-
-    """
-    Root query.
-    """
     type Query
-
-    """
-    Root mutation.
-    """
     type Mutation
 `
 
@@ -28,25 +20,9 @@ const schema = makeExecutableSchema({
     resolvers: [JournalResolvers]
 });
 
-
-// // Construct a schema, using GraphQL schema language
-// const schema = buildSchema(`
-//   type Query {
-//     tenants: [Tenant!]!
-//   }
-// `);
-
-// // The root provides a resolver function for each API endpoint
-// const root = {
-//   hello: () => {
-//     return 'Hello world!';
-//   },
-// };
-
 const app = express();
 app.use('/graphql', graphqlHTTP({
   schema: schema,
-//   rootValue: root,
   graphiql: true,
 }));
 app.listen(4000);
